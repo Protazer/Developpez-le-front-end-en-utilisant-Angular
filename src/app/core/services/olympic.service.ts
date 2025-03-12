ac8c4910326995e0ee9056bbcc90794848b1d02f
florian <florian.huberdeau@laposte.net>
2025-03-12 23:19:01 +0100

    refactor(services): refactor olympics services

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,65 +1,52 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { BehaviorSubject, delay, map, Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { IOlympicServiceState } from './olympics.service.type';
 import { IOlympicCountry } from '../models/Olympic';
-import { IPieChartDatas } from '../models/PieChart';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<IOlympicCountry[] | null | undefined>(
-    undefined
-  );
+  private olympics$ = new BehaviorSubject<
+    IOlympicServiceState<IOlympicCountry[]>
+  >({
+    data: undefined,
+    loading: true,
+  });
 
   constructor(private http: HttpClient) {}
 
-  loadInitialData() {
+  loadInitialData(): Observable<IOlympicCountry[]> {
     return this.http.get<IOlympicCountry[]>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
+      tap((value: IOlympicCountry[]) => {
+        this.olympics$.next({ data: value, loading: false });
+      }),
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
+        this.olympics$.next({ data: undefined, loading: false });
         return caught;
       })
     );
   }
 
   getOlympics() {
-    return this.olympics$.asObservable();
+    return this.olympics$.asObservable().pipe(delay(1000));
   }
 
-  getCountriesChartMedals(): Observable<IPieChartDatas[] | undefined> {
-    return this.olympics$.pipe(
-      map((countries) => {
-        return countries?.map(({ country, participations, id }) => ({
-          extra: { id: id },
-          name: country,
-          value: participations.reduce((acc, value) => {
-            return acc + value.medalsCount;
-          }, 0),
-        }));
-      })
-    );
-  }
-
-  getCountryDetailsById(id: number) {
-    return this.olympics$.pipe(
-      map((countries) => {
-        return countries
-          ?.filter((country) => country?.id === id)
-          .map((country) => ({
-            name: country.country,
-            series: country.participations.map((participation) => ({
-              name: String(participation.year),
-              value: participation.medalsCount,
-            })),
-          }));
-      })
+  getCountryById(
+    id: number
+  ): Observable<IOlympicServiceState<IOlympicCountry>> {
+    return this.olympics$.asObservable().pipe(
+      delay(1000),
+      map(({ loading, data }) => ({
+        loading,
+        data: data?.find((country) => country?.id === id),
+      }))
     );
   }
 }
