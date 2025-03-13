fc32eb47f640d3475f6bb473c2199dbf1437bbd8
florian <florian.huberdeau@laposte.net>
2025-03-13 01:31:16 +0100

    feat(header): Add header back button

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  imports: [NgIf, SharedModule],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  public pathSubscription?: Subscription;
+  public showBackLink: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.pathSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showBackLink = event.url.includes('/details/');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.pathSubscription?.unsubscribe();
+  }
+
+  handleOnClickBack(): void {
+    this.router.navigateByUrl('/');
+  }
 }
